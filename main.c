d8892bc6e00c6fbac642bc8a4175b971ed518e85
Aryan Kumar <rahuljaikarkumar7042@gmail.com>
2024-11-14 17:34:01 +0530

    A brainfuck interpreter

---
+++ main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define ARRAY_LENGTH 30000
+
+char ARRAY[ARRAY_LENGTH];
+int ARRAY_POINTER = 0;
+int NUM_BRACK = 0;
+int INS_PTR = 0;
+
+void evaluate_bf(char *expression, int brack_index)
+{
+
+   char current;
+
+   while ((current = expression[INS_PTR]) != 0)
+   {
+      // printf("%d ", INS_PTR);
+      
+      if (current == '>')
+      {
+         ARRAY_POINTER++;
+         INS_PTR++;
+      }
+      else if (current == '<')
+      {
+         ARRAY_POINTER--;
+         INS_PTR++;
+      }
+      else if (current == '+')
+      {
+         ARRAY[ARRAY_POINTER]++;
+         INS_PTR++;
+      }
+      else if (current == '-')
+      {
+         ARRAY[ARRAY_POINTER]--;
+         INS_PTR++;
+      }
+      else if (current == '.')
+      {
+         putchar(ARRAY[ARRAY_POINTER]);
+         INS_PTR++;
+      }
+      else if (current == ',')
+      {
+         ARRAY[ARRAY_POINTER] = (char)getchar();
+         INS_PTR++;
+      }
+      else if (current == '[')
+      {
+         NUM_BRACK++;
+         INS_PTR++;
+         evaluate_bf(expression, INS_PTR);
+         
+      }
+      else if (current == ']')
+      {
+         if(ARRAY[ARRAY_POINTER]==0) {
+            INS_PTR++;
+            NUM_BRACK--;
+            return;
+         }
+         INS_PTR=brack_index;         
+      }
+
+      continue;
+
+      
+   }
+   if(NUM_BRACK!=0) {
+
+      fprintf(stderr, "Not correct brackets");
+      NUM_BRACK=0;
+   }
+}
+int main(int argc, char const *argv[])
+{
+   for(int i=0;i<ARRAY_LENGTH;++i)
+      ARRAY[i]=0;
+   evaluate_bf("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", 0);
+   printf("\n");
+   
+   return 0;
+}
